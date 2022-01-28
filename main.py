fe91a92b28a148cebc46ac5d90781a43b492221c
kokoretsu <kel39219515@gmail.com>
2022-01-28 18:40:18 +0300

    init

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def echo_all(messadge):
     zeleniy = types.KeyboardButton(text="зеленый чай")
     startKboard.add(malinovi, zeleniy)
     bot.send_message(messadge.chat.id, "о каком чае хочешь послушать?", reply_markup=startKboard)
+
 @bot.message_handler(func=lambda m: m.text =="поговорить о кофе")
 def echo_all(messadge):
     startKboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
@@ -29,18 +30,6 @@ def echo_all(messadge):
     monarch = types.KeyboardButton(text="монарх")
     startKboard.add(neskafe, monarch)
     bot.send_message(messadge.chat.id, "о каком кофе желаешь поговорить?", reply_markup=startKboard)
-@bot.message_handler(func=lambda m: m.text =="малиновый чай")
-def url(message):
-    markup = types.InlineKeyboardMarkup()
-    viki_malinovi = types.InlineKeyboardButton(text='вот о малиновом чае!', url='https://teawiki.ru/chai/malinovyj-polza-i-vred/')
-    markup.add(viki_malinovi)
-    bot.send_message(message.chat.id, "Тыкай, там о малиновом чае <3", reply_markup=markup)
-@bot.message_handler(func=lambda m: m.text =="зеленый чай")
-def url(message):
-    markup = types.InlineKeyboardMarkup()
-    viki_zeleniy = types.InlineKeyboardButton(text='вот о зеленом чае!', url='https://ru.wikipedia.org/wiki/%D0%97%D0%B5%D0%BB%D1%91%D0%BD%D1%8B%D0%B9_%D1%87%D0%B0%D0%B9')
-    markup.add(viki_zeleniy)
-    bot.send_message(message.chat.id, "Тяпай, там о зеленом чае <3", reply_markup=markup)
 @bot.message_handler(func=lambda m: m.text =="нескафе")
 def url(message):
     markup = types.InlineKeyboardMarkup()
@@ -53,4 +42,22 @@ def url(message):
     viki_monarch = types.InlineKeyboardButton(text='вот о монарх!', url='https://ru.wikipedia.org/wiki/%D0%9C%D0%BE%D0%BD%D0%B0%D1%80%D1%85')
     markup.add(viki_monarch)
     bot.send_message(message.chat.id, "Тяпай, там о монарх! <3", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda m: m.text =="малиновый чай")
+def mch(message):
+    startKboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    cherniy = types.KeyboardButton(text="черный чай")
+    chernichny = types.KeyboardButton(text="черничный чай")
+    startKboard.add(cherniy, chernichny)
+    bot.send_message(message.chat.id, 'вот о малиновом чае! " https://teawiki.ru/chai/malinovyj-polza-i-vred/ "')
+
+@bot.message_handler(func=lambda m: m.text =="зеленый чай")
+def zch(message):
+    startKboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    onchan = types.KeyboardButton(text="чай ончан")
+    mint = types.KeyboardButton(text="мятный чай")
+    startKboard.add(onchan, mint)
+    bot.send_message(message.chat.id, 'вот о зеленом чае! "https://ru.wikipedia.org/wiki/%D0%97%D0%B5%D0%BB%D1%91%D0%BD%D1%8B%D0%B9_%D1%87%D0%B0%D0%B9" ')
+
 bot.polling()
